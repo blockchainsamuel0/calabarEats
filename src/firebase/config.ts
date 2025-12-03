75e41e8b96024697714912dc93aca1bd3ea06add
Kelvin <k43608085@gmail.com>
2025-12-03 03:53:10 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,4 +3,11 @@
 // The Firebase config will be populated by the Firebase service.
 // This is a placeholder to make sure the import in other files works.
 
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-3597770684-d9ae1",
+  "appId": "1:725326316216:web:01c923cdd12c5e53db6a4d",
+  "apiKey": "AIzaSyDiHvyDFp5V1HuCvdmYGLbcBWJ9pmgXuUw",
+  "authDomain": "studio-3597770684-d9ae1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "725326316216"
+};
